f4a5771ce98f275f0840502d6e525ac2386882c2
saurabhnikam1997 <nikam280197@gmail.com>
2020-11-05 14:19:07 +0530

    modifyin code for showing table

--- src/ticTacToeGame/Tictactoe.java
+++ src/ticTacToeGame/Tictactoe.java
@@ -5,6 +5,7 @@ import java.util.Scanner;
 public class Tictactoe {
 	static char[] board = new char[10];
 	private static char playchoice;
+	private static char compChoice;
 	private static Scanner userInput;
 	public static void main(String args[]) {
 		System.out.println("---Welcome to Tictactoe Game---");
@@ -12,58 +13,62 @@ public class Tictactoe {
 		Tictactoe.Board();
 		Tictactoe.Inputchoice();
 		Tictactoe.ShowBoard(board);
-		Tictactoe.SelectLocation(board);
-	
+		Tictactoe.SelectLocation(board);	
 }
 
-private static char[] SelectLocation(char[] board2) {
-	System.out.println("Select the index to place: "+ playchoice); 
-	int index = extracted().nextInt();
+	private static char[] SelectLocation(char[] board2) {
+		System.out.println("Select the index to place: "+ playchoice); 
+		int index = extracted().nextInt();
 		board2[index] = playchoice;
 		System.out.println(board2);
 		ShowBoard(board2);
 		
-	return board2;
-	
-}
-
-private static Scanner extracted() {
-	return new Scanner(System.in);
-}
-
-private static void ShowBoard(char[] board) {
-	System.out.println("TicTacToe Board:");
-	System.out.println("_____________");
-	System.out.println("|  "+board[1]+"|  "+board[2]+"|  "+board[3]+"|");
-	System.out.println("─────────────");
-	System.out.println("|  "+board[4]+"|  "+board[5]+"|  "+board[6]+"|");
-	System.out.println("─────────────");
-	System.out.println("|  "+board[7]+"|  "+board[8]+"|  "+board[9]+"|");
-	System.out.println("‾‾‾‾‾‾‾‾‾‾‾‾‾");
-	
-}
-
-
-private static void Inputchoice() {
-	userInput = extracted();
-	System.out.println("Choose 'X' OR 'O' as input: ");
-	
-	playchoice = userInput.next().toUpperCase().charAt(0);
-	
-	if(playchoice=='X') {
+		return board2;	
 	}
-	else if(playchoice=='O') {
+
+	private static Scanner extracted() {
+		return new Scanner(System.in);
 	}
-	else {
-		System.out.println("Wrong input");
-		Inputchoice();
-	}	
-}
-private static char[] Board() {
-	for (int i=1; i<board.length; i++) {
-		
-		board[i]=' ';
+
+	private static void ShowBoard(char[] board) {
+		System.out.println("TicTacToe Board:");
+		System.out.println("_____________");
+		System.out.println("|  "+board[1]+"|  "+board[2]+"|  "+board[3]+"|");
+		System.out.println("─────────────");
+		System.out.println("|  "+board[4]+"|  "+board[5]+"|  "+board[6]+"|");
+		System.out.println("─────────────");
+		System.out.println("|  "+board[7]+"|  "+board[8]+"|  "+board[9]+"|");
+		System.out.println("‾‾‾‾‾‾‾‾‾‾‾‾‾");
 	}
-	return board;
+
+	private static void Inputchoice() {
+		userInput = extracted();
+		System.out.println("Choose 'X' OR 'O' as input: ");
+		playchoice = userInput.next().toUpperCase().charAt(0);
+	
+		if(playchoice=='X') {
+			setCompChoice('O');			
+		}
+		else if(playchoice=='O') {
+			setCompChoice('X');
+		}
+		else {
+			System.out.println("Wrong input");
+			Inputchoice();
+		}	
+	}
+	private static char[] Board() {
+		for (int i=1; i<board.length; i++) {
+			board[i]=' ';
+		}
+		return board;
+	}
+
+	public static char getCompChoice() {
+		return compChoice;
+	}
+
+	public static void setCompChoice(char compChoice) {
+		Tictactoe.compChoice = compChoice;
 	}	
 }
