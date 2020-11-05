435d3b77365faf82283bc517489efcfff2e20874
saurabhnikam1997 <nikam280197@gmail.com>
2020-11-05 12:24:21 +0530

    Resetting a board

---
+++ src/ticTacToeGame/Tictactoe.java
@@ -0,0 +1,20 @@
+package ticTacToeGame;
+
+public class Tictactoe {
+static char[] board = new char[10];  
+public static void main(String args[]) {
+	System.out.println("Tictactoe Game---");
+	Board();
+}
+
+
+private static char[] Board() {
+	for (int i=1; i<board.length; i++) {
+		
+		board[i]=' ';
+	}
+	return board;
+		
+	
+	}	
+}
