ecf62b6bfa4d7e01d3d08fb8408d3b6651563d2e
saurabhnikam1997 <nikam280197@gmail.com>
2020-11-05 13:46:28 +0530

    Showing a board by extracting user input

--- src/ticTacToeGame/Tictactoe.java
+++ src/ticTacToeGame/Tictactoe.java
@@ -3,31 +3,32 @@ package ticTacToeGame;
 import java.util.Scanner;
 
 public class Tictactoe {
-static char[] board = new char[10];
-private static char playchoice;
-private static Scanner userInput;
-private static Tictactoe game;  
-public static void main(String args[]) {
-	System.out.println("---Welcome to Tictactoe Game---");
-	game = new Tictactoe();
-	Tictactoe.Board();
-	Tictactoe.Inputchoice();
-	Tictactoe.ShowBoard(board);
-	Tictactoe.SelectLocation(board);
+	static char[] board = new char[10];
+	private static char playchoice;
+	private static Scanner userInput;
+	public static void main(String args[]) {
+		System.out.println("---Welcome to Tictactoe Game---");
+		new Tictactoe();
+		Tictactoe.Board();
+		Tictactoe.Inputchoice();
+		Tictactoe.ShowBoard(board);
+		Tictactoe.SelectLocation(board);
 	
 }
 
 private static char[] SelectLocation(char[] board2) {
 	System.out.println("Select the index to place: "+ playchoice); 
-	Scanner userinput = new Scanner(System.in);
-	int index = userinput.nextInt();
-		char[] board = null;
+	int index = extracted().nextInt();
 		board2[index] = playchoice;
 		System.out.println(board2);
 		ShowBoard(board2);
 		
 	return board2;
 	
+}
+
+private static Scanner extracted() {
+	return new Scanner(System.in);
 }
 
 private static void ShowBoard(char[] board) {
@@ -44,7 +45,7 @@ private static void ShowBoard(char[] board) {
 
 
 private static void Inputchoice() {
-	userInput = new Scanner(System.in);
+	userInput = extracted();
 	System.out.println("Choose 'X' OR 'O' as input: ");
 	
 	playchoice = userInput.next().toUpperCase().charAt(0);
